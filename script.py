3e79e746258fc06d4d4e8bfc2f2832865e808033
lworkman <liam.workman@gmail.com>
2017-04-22 14:20:09 -0700

    Added in comments and split into functions

--- script.py
+++ script.py
@@ -1,42 +1,52 @@
 import requests
 import csv
-apiKey = 'ac83034cfa742c0f79c26e9a612b4ba7e2aa0d3d'
+import sys
 
-formIDRequest = requests.request('GET' , 'https://api.typeform.com/v1/forms?key='+apiKey)
-formID = formIDRequest.json()[0]['id']
+# Takes an API key on the command line and retrieves the first form tied to that API key. It then outputs all the answers as a CSV, with an added index
+# Created by Liam Workman
 
-formRequest = requests.request('GET' , 'https://api.typeform.com/v1/form/'+formID+'?key='+apiKey)
+def createDictionaryFromTypeformResponse(typeformResponse,questionKeys):
+    iterator = 0
+    holder = {}
+    for response in typeformResponse:
+        questionAnswers = {}
+        questionAnswers['token'] = response['token']
+        questionAnswers['key'] = iterator
+        iterator += 1
+        for questionID in questionKeys:
+            if questionID not in response['answers']:
+                questionAnswers[questionID] = 'NaN'
+            else:
+                questionAnswers[questionID] = response['answers'][questionID]
+        holder[response['token']] = questionAnswers
+    return holder
+
+def createCSVFromDict(headers,rows):
+    with open('./output/answers.csv','w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = headers)
+        writer.writeheader()
+
+        for row in rows:
+            writer.writerow(rows[row])
+
+def retrieveAllFormsFromTypeform(apiKey):
+
+    formIDRequest = requests.request('GET' , 'https://api.typeform.com/v1/forms?key='+apiKey)
+
+    formID = formIDRequest.json()[0]['id']
+    formRequest = requests.request('GET' , 'https://api.typeform.com/v1/form/'+formID+'?key='+apiKey)
+
+    return formRequest.json()
+
+# This is where the magic happens
+
+form = retrieveAllFormsFromTypeform(sys.argv[1])
 
 questionIDs = []
 
-for question in formRequest.json()['questions']:
+for question in form['questions']:
     questionIDs.append(question['id'])
 
-print(len(formRequest.json()['responses']))
+peoplesResponses = createDictionaryFromTypeformResponse(form['responses'],questionIDs)
 
-peoplesResponses = {}
-
-iterator = 0
-
-for response in formRequest.json()['responses']:
-    questionAnswers = {}
-    questionAnswers['token'] = response['token']
-    questionAnswers['key'] = iterator
-    iterator += 1
-    for questionID in questionIDs:
-        if questionID not in response['answers']:
-            questionAnswers[questionID] = float('NaN')
-        else:
-            questionAnswers[questionID] = response['answers'][questionID]
-    peoplesResponses[response['token']] = questionAnswers
-
-# for response in peoplesResponses:
-#     print(peoplesResponses[response])
-
-with open('./output/answers.csv','w') as csvfile:
-    fieldnames = ['key','token'] + questionIDs
-    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-    writer.writeheader()
-
-    for people in peoplesResponses:
-        writer.writerow(peoplesResponses[people])
+createCSVFromDict(['key','token']+questionIDs,peoplesResponses)
